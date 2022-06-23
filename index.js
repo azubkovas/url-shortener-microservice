f986f3e885c7df0587a52a2db5e868f316e465c4
Arsenijus Zubkovas <arsenijus.zubkovas@gmail.com>
2022-06-23 17:42:04 +0300

    Installed some packages, started working on the post request to /api/shorturl

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true});
+let urlPairSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+})
+let UrlPair = mongoose.model('UrlPair', urlPairSchema);
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +32,10 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res){
+  res.send(req.body);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);

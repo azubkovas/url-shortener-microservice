de6d3f20f13d550163b5b0a7c8089690adc0b060
Arsenijus Zubkovas <arsenijus.zubkovas@gmail.com>
2022-06-26 20:17:24 +0300

    Hopefully fulfilled the requirements

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const URL = require('url').URL;
+const dns = require('dns')
 const app = express();
 
 // Basic Configuration
@@ -12,7 +14,7 @@ const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true});
 let urlPairSchema = new mongoose.Schema({
   original_url: String,
-  short_url: Number
+  short_url: {type: Number, unique: true}
 })
 let UrlPair = mongoose.model('UrlPair', urlPairSchema);
 
@@ -34,7 +36,69 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.post('/api/shorturl', function(req, res){
-  res.send(req.body);
+  UrlPair.findOne().sort({_id: -1}).exec((err, data) => {
+    if(err)
+    {
+      console.log('failed to find any data');
+      console.log(err);
+    }
+    else
+    {
+      console.log('successfully found data, latest entry:');
+      console.log(data);
+      let shortUrl = data ? data.short_url + 1 : 1;
+      try 
+      {
+        let newUrl = new URL(req.body.url);
+        dns.lookup(newUrl.hostname, (err, address, family) => {
+          if(err)
+          {
+            console.log('dns lookup failed');
+            res.json({error: 'invalid url'});
+          }
+          else
+          {
+            let newPair = new UrlPair({original_url: req.body.url, short_url: shortUrl});
+            newPair.save((err, createdData) => {
+              if(err)
+              {
+                console.log('not successfull');
+                console.log(err);
+              }
+              else
+              {
+                console.log('successfull')
+                res.json({original_url: createdData.original_url, short_url: createdData.short_url});
+              }
+            })      
+          }
+        })
+      } 
+      catch (error) 
+      {
+        console.log('url parse failed');
+        res.json({error: 'invalid url'});
+      }
+    }
+  })
+})
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  let url;
+  UrlPair.findOne({short_url: Number(req.params.short_url)}, {}, {}, (err, data) => {
+    if(err)
+    {
+      res.json({error: 'Wrong format'});
+    }
+    else if(!data)
+    {
+      res.json({error: 'No corresponding url'});
+    }
+    else
+    {
+      res.redirect(data.original_url);
+    }
+  })
 })
 
 app.listen(port, function() {
